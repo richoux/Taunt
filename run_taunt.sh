0fca12520b6bed857ef274903edffe37682c417d
Florian Richoux <florian.richoux@polytechnique.edu>
2021-12-02 15:14:17 +0900

    Resources detection

--- run_taunt.sh
+++ run_taunt.sh
@@ -9,12 +9,14 @@ do
 		info=$(file "maps/${name}.jpg" | awk -F', ' '{print $(NF-1)}')
 		width=$(echo $info | awk -Fx '{print $1}')
 		height=$(echo $info | awk -Fx '{print $2}')
+
 		inkscape -D -o "maps/taunted/${name}_contour.png" -w $width -h $height "maps/taunted/${name}_contour.svg"
 		convert -composite "maps/${name}.jpg" "maps/taunted/${name}_contour.png" -gravity center "maps/taunted/${name}_contour.png"
-		#if [[ ! "$name" == *"LE" ]]
-		#then
+
 		inkscape -D -o "maps/taunted/${name}_height.png" -w $width -h $height "maps/taunted/${name}_height.svg"
 		convert -composite "maps/${name}.jpg" "maps/taunted/${name}_height.png" -gravity center "maps/taunted/${name}_height.png"
-		#fi
+
+		inkscape -D -o "maps/taunted/${name}_resources.png" -w $width -h $height "maps/taunted/${name}_resources.svg"
+		convert -composite "maps/${name}.jpg" "maps/taunted/${name}_resources.png" -gravity center "maps/taunted/${name}_resources.png"
 done
 
--- src/create_height_zones_svg.cpp
+++ src/create_height_zones_svg.cpp
@@ -17,7 +17,10 @@ int main( int argc, char* argv[] )
 	std::string mapfile = std::string( argv[1] );
 	std::string mapfile_height = mapfile;
 	mapfile_height.replace( mapfile_height.end()-4, mapfile_height.end(), "_height.txt" );
-	
+
+	std::string mapfile_resources = mapfile;
+	mapfile_resources.replace( mapfile_resources.end()-4, mapfile_resources.end(), "_resources.txt" );
+
 // std::ifstream in( "C:/Users/Flo/Documents/GitHub/Taunt/Release/map.txt" );
 	std::ifstream in( mapfile );
 	if( !in.is_open() )
@@ -33,18 +36,26 @@ int main( int argc, char* argv[] )
 		return EXIT_FAILURE;
 	}
 
+	std::ifstream in_resources( mapfile_resources );
+	if( !in_resources.is_open() )
+	{
+		std::cerr << "Can't open file " << mapfile_resources << "\n";
+		return EXIT_FAILURE;
+	}
+
 	std::vector< std::vector< int > > map_height;
+	std::vector< std::vector< bool > > map_resources;
 	std::vector< std::vector< int > > map_0;
 	std::vector< std::vector< int > > map_2;
 	std::vector< std::vector< int > > map_4;
 	std::vector< std::vector< int > > map_unbuildable;
 	int height = 0;
 	int width = 0;
-
+	
 	std::cout << "Read input file " << mapfile_height << "...\n";
 	std::string line;
 	char c;
-
+	
 	// Dealing with a SC2 map
 	if( mapfile.substr( mapfile.size() - 6, 6 ) == "LE.txt" )
 	{
@@ -58,15 +69,13 @@ int main( int argc, char* argv[] )
 			{
 				if( height == 0 )
 					++width;
-
+				
 				int h = static_cast<int>( std::round( std::stof( w ) ) );
 				if( h == 2 )
 					h = 0;
 				if( h == 3 )
 					h = 2;
-
-				std::cout << h;
-					
+				
 				switch( h )
 				{
 				case 0: // low
@@ -89,7 +98,6 @@ int main( int argc, char* argv[] )
 				}
 			}
 			++height;
-			std::cout << "\n";
 		}
 	}
 	else  // Dealing with a BW map
@@ -102,7 +110,7 @@ int main( int argc, char* argv[] )
 			{
 				if( height == 0 )
 					++width;
-
+				
 				switch( c )
 				{
 				case '0': // low
@@ -124,14 +132,31 @@ int main( int argc, char* argv[] )
 					map_height[ height ].push_back( 5 );
 				}
 			}
-
+			
 			++height;
 		}
 	}
 	in_height.close();
 
-	std::cout << "Read input file " << mapfile << "...\n";
+	std::cout << "Read input file " << mapfile_resources << "...\n";
 	int row = 0;
+	while( std::getline( in_resources, line ) )
+	{
+		map_resources.push_back( std::vector< bool >() );
+		std::stringstream ssi( line );
+		while( ssi >> c )
+		{
+			if( c == '0' ) [[likely]]
+				map_resources[ row ].push_back( false );
+			else
+				map_resources[ row ].push_back( true );
+		}
+		++row;
+	}
+	in_resources.close();
+
+	std::cout << "Read input file " << mapfile << "...\n";
+	row = 0;
 	while( std::getline( in, line ) )
 	{
 		int column = 0;
@@ -154,8 +179,13 @@ int main( int argc, char* argv[] )
 				map_0[ row ].push_back( 1 );
 				map_2[ row ].push_back( 1 );
 				map_4[ row ].push_back( 1 );
-				map_unbuildable[ row ].push_back( 2 );
-				// if( map_height[row][column] == 4 )
+
+				if( map_resources[row][column] )
+					map_unbuildable[ row ].push_back( 1 );
+				else
+					map_unbuildable[ row ].push_back( 2 );
+				
+        // if( map_height[row][column] == 4 )
 				// {	
 				// 	map_0[ row ].push_back( 1 );
 				// 	map_2[ row ].push_back( 1 );
@@ -222,6 +252,21 @@ int main( int argc, char* argv[] )
 
 	std::cout << "Map size (w x h): " << width << "x" << height << "\n";
 
+	for( int j = 0; j < height; ++j )
+		for( int i = 0; i < width; ++i )
+			if( map_resources[j][i] )
+			{
+				if( map_height[j][i] == 4 )
+					map_4[j][i] = 2;
+				else
+					if( map_height[j][i] == 2 )
+						map_2[j][i] = 2;
+					else
+						if( map_height[j][i] == 0 )
+							map_0[j][i] = 2;				
+			}
+
+	
 	std::string labeled_mapfile_0 = mapfile;
 	labeled_mapfile_0.replace( labeled_mapfile_0.begin(), labeled_mapfile_0.begin() + 5, "maps/taunted/" );
 	labeled_mapfile_0.replace( labeled_mapfile_0.end() - 4, labeled_mapfile_0.end(), "_0_low_labeled.txt" );
@@ -324,6 +369,35 @@ int main( int argc, char* argv[] )
 	labeled_out_unbuildable << ss_unbuildable.str();
 	labeled_out_unbuildable.close();
 
+	// assigning resources to zones
+	size_t number_zones_0 = simplified_0.size();
+	size_t number_zones_0_2 = simplified_0.size() + simplified_2.size();
+	size_t number_zones_0_2_4 = simplified_0.size() + simplified_2.size() + simplified_4.size();
+	std::vector< boost::geometry::model::multi_point<point> > resources( number_zones_0_2_4 );
+
+	std::vector< point > resource_points;
+	for( int j = 0 ; j < height ; ++j )
+		for( int i = 0 ; i < width ; ++i )
+			if( map_resources[j][i] )
+				resource_points.emplace_back( i, j );
+
+	//std::cout << "resource_points.size()=" << resource_points.size() << std::endl;
+	
+	for( size_t i = 0 ; i < number_zones_0 ; ++i )
+		for( auto& point : resource_points )
+			if( boost::geometry::within( point, simplified_0[ i ] ) || boost::geometry::intersects( point, simplified_0[ i ] ) )
+				boost::geometry::append( resources[ i ], point );
+
+	for( size_t i = 0 ; i < simplified_2.size() ; ++i )
+		for( auto& point : resource_points )
+			if( boost::geometry::within( point, simplified_2[ i ] ) || boost::geometry::intersects( point, simplified_2[ i ] ) )
+				boost::geometry::append( resources[ number_zones_0 + i ], point );
+
+	for( size_t i = 0 ; i < simplified_4.size() ; ++i )
+		for( auto& point : resource_points )
+			if( boost::geometry::within( point, simplified_4[ i ] ) || boost::geometry::intersects( point, simplified_4[ i ] ) )
+				boost::geometry::append( resources[ number_zones_0_2 + i ], point );
+	
 // std::vector< std::vector< bool > > contour_map( height );
 	std::string contour_mapfile = mapfile;
 	// contour_mapfile.replace( contour_mapfile.begin(), contour_mapfile.begin() + 5, "maps/taunted/" );
@@ -463,6 +537,10 @@ int main( int argc, char* argv[] )
 	contour_mapfile_svg_unbuildable.replace( contour_mapfile_svg_unbuildable.begin(), contour_mapfile_svg_unbuildable.begin() + 5, "maps/taunted/" );
 	contour_mapfile_svg_unbuildable.replace( contour_mapfile_svg_unbuildable.end() - 4, contour_mapfile_svg_unbuildable.end(), "_unbuildable.svg" );
 
+	std::string contour_mapfile_svg_resources = contour_mapfile;
+	contour_mapfile_svg_resources.replace( contour_mapfile_svg_resources.begin(), contour_mapfile_svg_resources.begin() + 5, "maps/taunted/" );
+	contour_mapfile_svg_resources.replace( contour_mapfile_svg_resources.end() - 4, contour_mapfile_svg_resources.end(), "_resources.svg" );
+
 	std::ofstream contour_svg( contour_mapfile_svg );
 	boost::geometry::svg_mapper<point> mapper( contour_svg, 9*width, 9*height );
 
@@ -477,6 +555,9 @@ int main( int argc, char* argv[] )
 
 	std::ofstream contour_svg_unbuildable( contour_mapfile_svg_unbuildable );
 	boost::geometry::svg_mapper<point> mapper_unbuildable( contour_svg_unbuildable, 9*width, 9*height );
+
+	std::ofstream contour_svg_resources( contour_mapfile_svg_resources );
+	boost::geometry::svg_mapper<point> mapper_resources( contour_svg_resources, 9*width, 9*height );
 
 	for( size_t i = 0; i < simplified_0.size(); ++i )
 	{
@@ -524,6 +605,113 @@ int main( int argc, char* argv[] )
 	{
 		mapper.map( simplified_unbuildable[i], "fill-opacity:0.5;fill:rgb(0,0,255);stroke:rgb(0,0,0);stroke-width:5");
 		mapper_unbuildable.map( simplified_unbuildable[i], "fill-opacity:0.5;fill:rgb(53,255,0);stroke:rgb(0,0,0);stroke-width:5");
+	}
+
+	boost::geometry::model::box<point> box;
+	std::vector< boost::geometry::model::box<point> > boxes;
+
+	for( auto& mp : resources )
+	{
+		//std::cout << "mp.size()=" << boost::geometry::num_points( mp ) << std::endl;
+		boost::geometry::clear( box );
+		if( !boost::geometry::is_empty( mp ) )
+		{
+			boost::geometry::envelope( mp, box);
+			mapper_resources.add( box );
+			boxes.push_back( box );
+			//std::cout << "Box: " << boost::geometry::dsv(box) << std::endl;
+		}
+	}
+	
+	// invert y-axis for the box SVG file from SC1 maps (?!),
+	if( mapfile.substr( mapfile.size() - 6, 6 ) != "LE.txt" )
+		for( auto& box : boxes )
+		{
+			boost::geometry::set<boost::geometry::min_corner, 1>( box, height-boost::geometry::get<boost::geometry::min_corner, 1>( box ) );
+			boost::geometry::set<boost::geometry::max_corner, 1>( box, height-boost::geometry::get<boost::geometry::max_corner, 1>( box ) );
+		}
+	
+	// boost::geometry::model::multi_polygon< boost::geometry::model::polygon<point> > mp;
+		
+	// for( size_t i = 0; i < simplified_0.size(); ++i )
+	// {
+	// 	boost::geometry::clear( mp );
+	// 	boost::geometry::clear( box );
+	// 	mp.resize( simplified_0[i].inners().size() );
+	// 	for( size_t j = 0; j < simplified_0[i].inners().size(); ++j )
+	// 		mp[j].outer() = simplified_0[i].inners()[j];			
+
+	// 	if( !boost::geometry::is_empty( mp ) )
+	// 	{
+	// 		boost::geometry::correct( mp );
+	// 		boost::geometry::envelope( mp, box);
+	// 		mapper_resources.add( box );
+	// 		boxes.push_back( box );
+	// 		// std::cout << "MP: " << boost::geometry::dsv(mp) << std::endl;
+	// 		// std::cout << "Box: " << boost::geometry::dsv(box) << std::endl;
+	// 	}
+	// }
+
+	// for( size_t i = 0; i < simplified_2.size(); ++i )
+	// {
+	// 	boost::geometry::clear( mp );
+	// 	boost::geometry::clear( box );
+	// 	mp.resize( simplified_2[i].inners().size() );
+	// 	for( size_t j = 0; j < simplified_2[i].inners().size(); ++j )
+	// 		mp[j].outer() = simplified_2[i].inners()[j];			
+
+	// 	if( !boost::geometry::is_empty( mp ) )
+	// 	{
+	// 		boost::geometry::correct( mp );
+	// 		boost::geometry::envelope( mp, box);
+	// 		mapper_resources.add( box );
+	// 		boxes.push_back( box );
+	// 		// std::cout << "MP: " << boost::geometry::dsv(mp) << std::endl;
+	// 		// std::cout << "Box: " << boost::geometry::dsv(box) << std::endl;
+	// 	}
+	// }
+
+	// for( size_t i = 0; i < simplified_4.size(); ++i )
+	// {
+	// 	boost::geometry::clear( mp );
+	// 	boost::geometry::clear( box );
+	// 	mp.resize( simplified_4[i].inners().size() );
+	// 	for( size_t j = 0; j < simplified_4[i].inners().size(); ++j )
+	// 		mp[j].outer() = simplified_4[i].inners()[j];			
+
+	// 	if( !boost::geometry::is_empty( mp ) )
+	// 	{
+	// 		boost::geometry::correct( mp );
+	// 		boost::geometry::envelope( mp, box);
+	// 		mapper_resources.add( box );
+	// 		boxes.push_back( box );
+	// 		// std::cout << "MP: " << boost::geometry::dsv(mp) << std::endl;
+	// 		// std::cout << "Box: " << boost::geometry::dsv(box) << std::endl;
+	// 	}
+	// }
+
+	for( size_t i = 0; i < boxes.size(); ++i )
+	{
+		boost::geometry::correct( boxes[i] );
+		int box_width = std::abs( boost::geometry::get<boost::geometry::min_corner, 0>(  boxes[i] ) - boost::geometry::get<boost::geometry::max_corner, 0>( boxes[i] ) );
+		int box_height = std::abs( boost::geometry::get<boost::geometry::min_corner, 1>( boxes[i] ) - boost::geometry::get<boost::geometry::max_corner, 1>( boxes[i] ) );
+		int box_area = boost::geometry::area( boxes[i] );
+		
+		//if( boost::geometry::area( boxes[i] ) <= 260 ) // In tested maps, the area of resources is usually around 200 (a bit more on SC2)
+		if( ( box_width < 2 * box_height && box_height < 2 * box_width && box_area <= 500 ) || box_area <= 150 )
+			mapper_resources.map( boxes[i], "fill-opacity:0.5;fill:rgb(0,0,255);stroke:rgb(0,0,0);stroke-width:5");
+		else
+			mapper_resources.map( boxes[i], "fill-opacity:0.5;fill:rgb(255,0,0);stroke:rgb(0,0,0);stroke-width:5");
+
+		point center;
+		boost::geometry::centroid( boxes[i], center );
+		boost::geometry::set<0>( center, center.x() - 3 );
+		boost::geometry::set<1>( center, center.y() - 1 );
+		int area = boost::geometry::area( boxes[i] );
+		std::string area_string = std::to_string( area );
+		mapper_resources.text( center,
+		                       area_string,
+		                       "fill-opacity:1;fill:rgb(0,255,0);font-size:42" );
 	}
 	
 	return EXIT_SUCCESS;
